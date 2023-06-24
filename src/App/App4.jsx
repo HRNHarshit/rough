6e23eab0baefcde41db6a5a1c3dc864e9755c134
HRNHarshit <hrnharshit@gmail.com>
2023-06-24 12:45:08 +0530

    changes

--- src/App/App4.jsx
+++ src/App/App4.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const App4 = () => {
+    const [num, setNum] = useState(0);
+    const [nums, setNums] = useState(0);
+
+    useEffect(() => {
+        alert("I'm Clicked");
+        console.log("I'm licked");
+    }, [num]);
+
     return (
         <>
-            <div></div>
+            <button
+                className="btn"
+                onClick={() => {
+                    setNum(num + 1);
+                }}
+            >
+                <h1> Click me {num} </h1>
+            </button>
+            <br />
+            <button
+                className="btn"
+                onClick={() => {
+                    setNums(nums + 1);
+                }}
+            >
+                <h1> Click me {nums} </h1>
+            </button>
         </>
     );
 };
